ca72e8270297141a9a2735b0b9e94098558f436b
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-04-12 18:39:12 +0530

    calculator webapp using POST method python

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+app = Flask("mytest")
+
+
+@app.route('/')
+def home():
+    return "<b> My Home Page </b>"
+
+
+@app.route('/survey')
+def form():
+    formrender = render_template("survey.html")
+    return formrender
+
+# Get Request Method
+
+# @app.route('/req')
+# def req():
+#     if request.method == "GET":
+#         data = request.args.get("name")
+#         return data
+
+
+@app.route('/req', methods=['POST'])
+def req():
+    if request.method == "POST":
+        data = request.form.get("name")
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        operend = request.form.get("operation")
+        if operend == "add":
+            number = int(num1) + int(num2)
+        elif operend == "sub":
+            number = int(num1) - int(num2)
+        elif operend == "mul":
+            number = int(num1) * int(num2)
+        elif operend == "div":
+            number = int(num1) / int(num2)
+        formrender = render_template(
+            "survey.html", value=str(number), name=data)
+        print(data)
+        return formrender
+
+
+@app.route('/req/<name>')
+def dynrou(name):
+    return name.lower()
+
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
+
+app.run(host="0.0.0.0", port=80, debug=True)
